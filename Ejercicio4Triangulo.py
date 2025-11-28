96679b1c1a50fef4e10266925ecb79c04af13948
Pabloceda <pabloceda00@gmail.com>
2025-11-28 17:56:44 +0100

    Modificaciones

--- Ejercicio4Triangulo.py
+++ Ejercicio4Triangulo.py
@@ -2,7 +2,7 @@ altura = int(input("Introduce la altura de la escalera: "))
 for i in range(1, altura + 1):
     for j in range(1, altura + 1):
         if i == altura or i == j or j == 1:
-            print("4", end=" ")
+            print("*", end=" ")
         else:
             print(" ", end=" ")
     print()
